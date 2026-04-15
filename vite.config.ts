695eedd722cc519a0d2b14dfb768120cacff71b5
Moti <aminqp@gmail.com>
2026-04-15 20:21:32 +0100

    chore: fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 // Replace 'web-worker-manager' with your actual GitHub repo name
 export default defineConfig({
-  base: '/',
+  base: '/web-worker-manager/',
 });
